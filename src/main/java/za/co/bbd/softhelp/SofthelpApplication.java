c495ba2353144eb5f0dd5f4896995683efb34663
kgotlelelo-cmd <kmasenam@student.wethinkcode.co.za>
2021-05-10 11:14:49 +0200

    initializer

---
+++ src/main/java/za/co/bbd/softhelp/SofthelpApplication.java
@@ -0,0 +1,13 @@
+package za.co.bbd.softhelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofthelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofthelpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/bbd/softhelp/SofthelpApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.bbd.softhelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofthelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
