5f771c1174ca79ca139e9c0dd6d583f8f4650f0b
Ethan Alberts <ethana@bbd.co.za>
2021-05-11 15:50:54 +0200

    Saving Progress

--- src/main/java/za/co/bbd/softhelp/Repository/ProjectRepository.java
+++ src/main/java/za/co/bbd/softhelp/Repository/ProjectRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import za.co.bbd.softhelp.Models.ProjectTable;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ProjectRepository extends JpaRepository<ProjectTable,Long> {
+    List<ProjectTable> findBySkillId(Long skillId);
 }
