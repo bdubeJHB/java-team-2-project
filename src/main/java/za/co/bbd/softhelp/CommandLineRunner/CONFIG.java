4d6a97718c1aa67d88cb99b1aea5798f58f10869
pimahlan <pimahlan@student.wethinkcode.com>
2021-05-13 10:10:38 +0200

    b

--- src/main/java/za/co/bbd/softhelp/CommandLineRunner/CONFIG.java
+++ src/main/java/za/co/bbd/softhelp/CommandLineRunner/CONFIG.java
@@ -35,33 +35,40 @@ public class CONFIG {
             SkillsCategory skillsCategory2 = new SkillsCategory("Python");
             SkillsCategory skillsCategory3 = new SkillsCategory("C#");
             SkillsCategory skillsCategory4 = new SkillsCategory("PHP");
+
             // -----------End of Config  ----------------------------
 
             // ----------- Test database -----------------------------
             Client user = new Client("John","last","john@Last");
             Client user1 = new Client("Simon","last","simon@Last");
             Client user2 = new Client("smith","last","smith@Last");
+            userRepository.save(user);
+            skillsCategory1.setUser(List.of(user));
+            skillsRepository.saveAll(List.of(skillsCategory1));
 
-            skillsCategory1.setUser(List.of(user,user1));
-            skillsCategory2.setUser(List.of(user2,user1));
-            skillsCategory3.setUser(List.of(user,user1));
 
-            ProjectTable projectTable = new ProjectTable("website", 123.0F);
-            projectTable.setUser(user);
-            projectTable.setStatus(status1);
-            projectTable.setSkill(skillsCategory2);
-            projectTable.setWorker(user2);
+//            skillsCategory1.setUser(List.of(user,user1));
+//            skillsCategory2.setUser(List.of(user2,user1));
+//            skillsCategory3.setUser(List.of(user,user1));
 
-            ProjectTable projectTable2 = new ProjectTable("mobile app", 133.0F);
-            projectTable2.setUser(user2);
-            projectTable2.setStatus(status2);
-            projectTable2.setSkill(skillsCategory3);
-            projectTable2.setWorker(user1);
 
-            userRepository.saveAll(List.of(user1,user2,user));
-            skillsRepository.saveAll(List.of(skillsCategory1,skillsCategory2,skillsCategory3));
-            statusRepository.saveAll(List.of(status1,status2,status3));
-            projectRepository.saveAll(List.of(projectTable,projectTable2));
+
+//            ProjectTable projectTable = new ProjectTable("website", 123.0F);
+//            projectTable.setUser(user);
+//            projectTable.setStatus(status1);
+//            projectTable.setSkill(skillsCategory2);
+//            projectTable.setWorker(user2);
+//
+//            ProjectTable projectTable2 = new ProjectTable("mobile app", 133.0F);
+//            projectTable2.setUser(user2);
+//            projectTable2.setStatus(status2);
+//            projectTable2.setSkill(skillsCategory3);
+//            projectTable2.setWorker(user1);
+
+//            userRepository.saveAll(List.of(user1,user2,user));
+//            skillsRepository.saveAll(List.of(skillsCategory1,skillsCategory2,skillsCategory3,skillsCategory0,skillsCategory4));
+//            statusRepository.saveAll(List.of(status1,status2,status3));
+//            projectRepository.saveAll(List.of(projectTable,projectTable2));
 
 //            System.out.println(projectRepository.count());
             // --------End of test -----------------------------------
